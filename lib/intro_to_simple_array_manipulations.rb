a42aa518b722a9a9568b818b3fbe8a75c7371d6e
Jonathan Yu <jonathan.yu.jwy2@yale.edu>
2021-06-02 18:40:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,14 @@
 def using_push(array, string)
   return array.push(string)
 end
+
+def using_unshift(array, string)
+  return array.unshift(string)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return 
